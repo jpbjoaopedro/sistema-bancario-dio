d7933e55337c2d4d9c9a18869cf2e40dc942c5ff
João Pedro Bandeira <jpbandeiradossantos@gmail.com>
2025-09-02 15:23:50 -0300

    entrega

--- desafio.py
+++ desafio.py
@@ -191,7 +191,6 @@ def recuperar_conta_cliente(cliente):
         print("\n@@@ Cliente não possui conta! @@@")
         return
 
-    # FIXME: não permite cliente escolher a conta
     return cliente.contas[0]
 
 
